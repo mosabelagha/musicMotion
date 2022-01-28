f7e99b0584c6976855897bce118bd3b08bd6fe14
Mosab Elagha <mosab_elagha@apple.com>
2022-01-28 12:59:53 -0800

    Initial Commit

---
+++ Motion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Motion
+//
+//  Created by Mosab Elagha on 1/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Motion/MotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionApp.swift
+//  Motion
+//
+//  Created by Mosab Elagha on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct MotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
